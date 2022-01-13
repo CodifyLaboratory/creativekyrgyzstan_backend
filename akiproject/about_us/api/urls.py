b7c97e917a88bd7916ffeae8d309a3cb87bb317d
gulshat <arkytbekovagulshat@gmail.com>
2022-01-13 21:57:38 +0600

    Swagger for api in proccess

--- akiproject/about_us/api/urls.py
+++ akiproject/about_us/api/urls.py
@@ -1,7 +1,9 @@
 from rest_framework import routers, urlpatterns
-from .views import EventViewSet
+from .views import AboutUsViewSet, ReportsViewSet, SupervisoryViewSet
 
-router = routers.DefaultRouter()
-router.register('api/about', EventViewSet, 'about_us')
+router = routers.SimpleRouter()
+router.register('api/about', AboutUsViewSet, 'about_us')
+router.register('api/supervisory', SupervisoryViewSet, 'members')
+router.register('api/reports', ReportsViewSet, 'reports')
 
 urlpatterns = router.urls
