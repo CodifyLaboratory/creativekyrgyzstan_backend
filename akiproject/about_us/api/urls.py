e7ffda56345951d3609b4007f3f01235be5f43d5
gulshat <arkytbekovagulshat@gmail.com>
2022-01-13 23:20:40 +0600

    added api for model AboutUS

--- akiproject/about_us/api/urls.py
+++ akiproject/about_us/api/urls.py
@@ -2,8 +2,8 @@ from rest_framework import routers, urlpatterns
 from .views import AboutUsViewSet, ReportsViewSet, SupervisoryViewSet
 
 router = routers.SimpleRouter()
-router.register('api/about', AboutUsViewSet, 'about_us')
-router.register('api/supervisory', SupervisoryViewSet, 'members')
+router.register('api/about', AboutUsViewSet, 'about')
+router.register('api/supervisory', SupervisoryViewSet, 'supervisory')
 router.register('api/reports', ReportsViewSet, 'reports')
 
 urlpatterns = router.urls
