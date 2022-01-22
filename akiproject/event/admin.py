15cc0b31836db3280231ab0333e1594e2f108887
TrueRealHero <ub1nono@gmail.com>
2022-01-23 01:02:37 +0600

    inlines for images

--- akiproject/event/admin.py
+++ akiproject/event/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.forms import inlineformset_factory
 
 # Register your models here.
 
 from .models import Event, EventImages
 
+# admin.site.register(Event)
+# admin.site.register(EventImages)
 
+class EventImagesAdmin(admin.StackedInline):
+    model = EventImages
 
-admin.site.register(Event)
-admin.site.register(EventImages)
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    inlines = [EventImagesAdmin]
+
+    class Meta:
+        model = Event
