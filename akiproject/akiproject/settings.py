37f6444a75fcf7816d4eef9521df70e49f33ffd7
TrueRealHero <ub1nono@gmail.com>
2022-02-19 16:16:33 +0600

    fixing bugs 6

--- akiproject/akiproject/settings.py
+++ akiproject/akiproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'mainpage',
     'association_member',
     'about_us',
-    'submit'
+    'submit',
 ]
 
 MIDDLEWARE = [
@@ -154,3 +154,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+django_heroku.settings(locals())
