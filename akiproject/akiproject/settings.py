fc3bdbcbf674a849d20b31b5d253c3af11f96d22
Gulshat <47497258+gulshat1705@users.noreply.github.com>
2022-02-25 14:38:57 +0600

    Update settings.py

--- akiproject/akiproject/settings.py
+++ akiproject/akiproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os, django_heroku
 # import environ
 
 # env = environ.Environ(
@@ -168,4 +167,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-django_heroku.settings(locals())
