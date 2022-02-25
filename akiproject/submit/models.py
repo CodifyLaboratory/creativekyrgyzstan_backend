8ff25e040d0610eb8233a78aab065fb8493709fa
Gulshat <47497258+gulshat1705@users.noreply.github.com>
2022-02-25 14:38:34 +0600

    added additional fields for social networks

--- akiproject/submit/models.py
+++ akiproject/submit/models.py
@@ -2,18 +2,23 @@ from django.db import models
 # Create your models here.
 
 class Submit(models.Model):
-    company_name = models.CharField('Company name', max_length=200, null= True)
-    full_name = models.CharField('Full name', max_length=100, null= True)
-    position = models.CharField('Position in company', max_length=100, null= True, blank= True)
-    company_reg_date = models.CharField('Company registration date', max_length=50, null= True)
-    company_members = models.IntegerField('Company members', null= True, blank= True)
-    company_webpage = models.CharField('Company webpage', max_length=100, null= True, blank= True)
-    company_socials = models.CharField('Company social pages', max_length=200, null= True, blank= True)
-    company_logo = models.ImageField(upload_to='images/logo', null= True, blank= True)
-    company_field = models.CharField('Company field', max_length=500, null= True)
-    company_activity = models.CharField('Company activity', max_length=500, null= True)
-    company_email = models.EmailField()
-    company_phone = models.CharField('Company phone number', max_length=100, null= True)
+    company_name = models.CharField('Полное наименование', max_length=200, null= True)
+    full_name = models.CharField('ФИО руководителя', max_length=100, null= True)
+    position = models.CharField('Должность руководителя', max_length=100, null= True, blank= True)
+    company_reg_date = models.DateField('Дата регистрации компании/ИП ', max_length=50, null= True)
+    company_members = models.IntegerField('Количество сотрудников', null= True, blank= True)
+    company_webpage = models.URLField('Сайт компании/ИП', max_length=100, null= True, blank= True)
+    facebook = models.CharField('Facebook', max_length=150, null= True, blank= True)
+    telegram = models.CharField('Telegram', max_length=150, null= True, blank= True)
+    whatsapp = models.CharField('WhatsApp', max_length=150, null= True, blank= True)
+    instagram = models.CharField('instagram', max_length=150, null= True, blank= True)
+    twitter = models.CharField('Twitter', max_length=150, null= True, blank= True)
+    tiktok = models.CharField('TikTok', max_length=150, null= True, blank= True)
+    company_logo = models.ImageField('Логотип', upload_to='images/logo', null= True, blank= True)
+    company_field = models.CharField('Креативная отрасль компании', max_length=500, null= True)
+    company_activity = models.CharField('Деятельность компании', max_length=500, null= True)
+    company_email = models.EmailField('Почта')
+    company_phone = models.CharField('Телефон', max_length=100, null= True)
 
     def __str__(self):
         return self.company_name
