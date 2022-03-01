92e27bee66efac7d3f58a0c3a76e13f98087fbfe
Gulshat <47497258+gulshat1705@users.noreply.github.com>
2022-03-01 15:39:50 +0600

    search for members

--- akiproject/association_member/admin.py
+++ akiproject/association_member/admin.py
@@ -1,30 +1,13 @@
-from dataclasses import fields
 from django.contrib import admin
 from .models import RulesAndPolitics, Members
-from submit.models import Submit
-from django import forms
-
-# class SubmitInline(admin.StackedInline):
-#     model = Submit
-
-
-
-class MembersForm(forms.ModelForm):
-
-    class Meta:
-        model = Members
-        fields = '__all__'
 
 
 admin.site.register(RulesAndPolitics)
-
-
 class MembersAdmin(admin.ModelAdmin):
-    # list_display = ('submit.full_name', )
-    list_select_related = (
-        'submit',
-    )
+    search_fields = ('submit__full_name',)
+
+
     
-    form = MembersForm
+
 
 admin.site.register(Members, MembersAdmin)
