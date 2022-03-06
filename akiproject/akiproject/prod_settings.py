e30821ae0ee930757db9c73b600f331dad6f3dc5
TrueRealHero <ub1nono@gmail.com>
2022-03-06 21:45:04 +0600

    Разделение файла settings.py

---
+++ akiproject/akiproject/prod_settings.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-!c-pnku_!1hi8&eu20vgasd8hAS7$@9tfdohhg=0fl0c4r^2l'
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1',]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_aki',
+        'USER': 'postgres',
+        'PASSWORD': '2022kreativ',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [ STATIC_DIR ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
